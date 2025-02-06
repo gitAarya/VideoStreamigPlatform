ab720797d72bfce2115edd3b0e7aa91d245ac888
gitAarya <aaryagithub@gmail.com>
2025-02-06 21:11:35 +0545

    routes added

--- src/app.js
+++ src/app.js
@@ -13,4 +13,11 @@ app.use(express.static("public"))
 
 app.use(cookieParser())
 
+
+//import routes
+ import userRouter  from "./routes/user.routes.js"
+
+ //routes declaration
+ app.use("/api/v1/users",userRouter)
+ //http://localhost:8000/api/v1/user/register
 export {app}
