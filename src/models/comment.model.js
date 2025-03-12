a5250203afb49528038bb9eb91627ba4f943d151
gitAarya <aaryagithub@gmail.com>
2025-03-12 20:55:46 +0545

    added video controller and routes; updated comment model for pagination

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,5 +1,5 @@
 import mongoose,{Schema} from "mongoose";
-
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const commentSchema=new Schema(
     {
@@ -18,4 +18,6 @@ const commentSchema=new Schema(
     },
     {timestamps:true}
 )
+commentSchema.plugin(mongooseAggregatePaginate)
+
 export const Comment=mongoose.model("Comment",commentSchema)
